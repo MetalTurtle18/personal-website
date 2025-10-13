f04ee2e35356da8f5eb983d2a3601d3587bb9160
MetalTurtle18 <dekolis1218@gmail.com>
2025-10-13 02:00:30 -0400

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+// eslint.config.js
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import svelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import astro from 'eslint-plugin-astro';
+import astroParser from 'astro-eslint-parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+
+  // TypeScript & JS
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+    },
+    plugins: { '@typescript-eslint': ts },
+    rules: {
+      ...ts.configs.recommended.rules,
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+
+  // Svelte
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+    plugins: { svelte },
+    rules: {
+      ...svelte.configs.recommended.rules,
+    },
+  },
+
+  // Astro
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+    plugins: { astro },
+    rules: {
+      ...astro.configs.recommended.rules,
+      'astro/no-unused-css-selector': 'off',
+    },
+  },
+
+  // Disable formatting conflicts
+  prettier,
+];
