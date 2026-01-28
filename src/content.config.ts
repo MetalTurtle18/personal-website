6cd3542f9633f3d84c8bcde80c1ba618b93ed7e6
MetalTurtle18 <dekolis1218@gmail.com>
2026-01-28 23:12:10 +0800

    Update specs and add date fields to schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,7 +7,9 @@ const contentSchema = z.object({
   title: z.string(),
   description: z.string(),
   summary: z.string().optional(),
-  pubDate: z.coerce.date(),
+  pubDate: z.coerce.date(), // The initial date of publication for this article
+  updateDate: z.coerce.date().optional(), // The date of the last update to this article
+  chronoDate: z.coerce.date().optional(), // The date that the content takes place (e.g., project completion date)
   author: z.string().default('Dorian Kolis'),
   featured: z.boolean().default(false),
   image: z
--- src/content/blog/post-1.md
+++ src/content/blog/post-1.md
@@ -7,7 +7,6 @@ featured: false
 image:
   url: 'https://docs.astro.build/assets/rose.webp'
   alt: 'The Astro logo on a dark background with a pink glow.'
-tags: ['astro', 'blogging', 'learning in public']
 ---
 
 # My First Blog Post
